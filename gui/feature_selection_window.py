0033a8ba9f2e39db5571c9169a5cf527bbe29261
liorpiz <liorpezman@gmail.com>
2020-03-02 17:40:14 +0200

    [FIX] Back window on load model process

--- gui/feature_selection_window.py
+++ gui/feature_selection_window.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 from gui.checkbox import Checkbar
 from gui.utils.helper_methods import load_feature_selection_list
+from utils.shared.input_settings import input_settings
 
 
 class FeatureSelectionWindow(tk.Frame):
@@ -16,11 +17,9 @@ class FeatureSelectionWindow(tk.Frame):
 
         self.feature_selection_methods = Checkbar(self, load_feature_selection_list())
 
-        self.back_button = tk.Button(self, text="Back",
-                                     command=lambda: controller.show_frame("AlgorithmsWindow"))
+        self.back_button = tk.Button(self, text="Back", command=self.back_window)
 
-        self.next_button = tk.Button(self, text="Next",
-                                     command=lambda: controller.show_frame("SimilarityFunctionsWindow"))
+        self.next_button = tk.Button(self, text="Next", command=self.next_window)
 
         # Layout using grid
         self.feature_selection_title.grid(row=0, column=2, pady=3)
@@ -29,3 +28,13 @@ class FeatureSelectionWindow(tk.Frame):
         self.grid_rowconfigure(13, minsize=100)
         self.back_button.grid(row=50, column=2, pady=3)
         self.next_button.grid(row=50, column=15, pady=3)
+
+    def back_window(self):
+        new_model_running = input_settings.get_new_model_running()
+        if new_model_running:
+            self.controller.show_frame("AlgorithmsWindow")
+        else:
+            self.controller.show_frame("ExistingAlgorithmsWindow")
+
+    def next_window(self):
+        self.controller.show_frame("SimilarityFunctionsWindow")
