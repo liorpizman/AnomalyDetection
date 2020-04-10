663d3ff3a91eaabeff7df545f91520e7b78dac7a
yehudapashay <yehudapashay1@gmail.com>
2020-04-10 12:07:00 +0300

    test ARIMA and VARMAX execution

--- scripts/models_test.py
+++ scripts/models_test.py
@@ -1,9 +1,12 @@
 import pandas as pd
+from numpy import concatenate
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import MaxAbsScaler
 from sklearn.linear_model import LogisticRegression, LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.statespace.varmax import VARMAX
 
 
 def run_isolation_forest(file_path):
@@ -95,7 +98,49 @@ def run_MLP_model(file_path):
     multi_pred = model.predict(X_train)
 
 
+def run_ARIMA_model(train_file_path, test_file_path):
+    df_train = pd.read_csv(f'{train_file_path}/without_anom.csv')
+    features_list = ['third_dis', 'Direction', 'Speed']
+    df_train = df_train[features_list]
+
+    scalar = MaxAbsScaler()
+
+    X_train = scalar.fit_transform(df_train)
+
+    X_Direction_train = X_train[:, 0]
+
+    Y_Direction_train = X_train[:, 0]
+
+    history = [x for x in X_Direction_train]
+
+    predictions = list()
+
+    for t in range(len(Y_Direction_train)):
+        model = ARIMA(history, order=(10, 1, 0))
+        model_fit = model.fit(disp=0)
+        output = model_fit.forecast()
+        yhat = output[0]
+        predictions.append(yhat[0])
+        obs = Y_Direction_train[t]
+        history.append(obs)
+        print('index: %d , predicted= %f , expected= %f , difference= %f' % (t, yhat, obs, abs(obs - yhat)))
+
+    model_varmax(X_train)
+
+    a = 1
+
+
+def model_varmax(X_test):
+    model = VARMAX(X_test, order=(1, 1))
+    model_fit = model.fit(disp=-1)
+    print(model_fit.summary().tables[1])
+    predictions = model_fit.forecast(steps=10)
+    print(predictions)
+
+
 path = "C:\\Users\\Yehuda Pashay\\Desktop\\fligth_data\\data_set\\train\\chicago_to_guadalajara"
 
+test_path = "C:\\Users\\Yehuda Pashay\\Desktop\\fligth_data\\data_set\\test\\chicago_to_guadalajara\\fore_attack"
 # run_logistic_regression(path)
-run_MLP_model(path)
+# run_MLP_model(path)
+run_ARIMA_model(path, test_path)
