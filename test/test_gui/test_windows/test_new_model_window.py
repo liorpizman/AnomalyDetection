3c1afcc02b927fe37917495ca636b49f1fada3ba
liorpiz <liorpezman@gmail.com>
2020-05-09 19:50:14 +0300

    update

--- test/test_gui/test_windows/test_new_model_window.py
+++ test/test_gui/test_windows/test_new_model_window.py
@@ -1,8 +1,16 @@
+'''
+Anomaly Detection of GPS Spoofing Attacks on UAVs
+Authors: Lior Pizman & Yehuda Pashay
+GitHub: https://github.com/liorpizman/AnomalyDetection
+DataSets: 1. ADS-B dataset 2. simulated data
+---
+Tests for new model window
+'''
+
 import unittest
+
 from tkinter import END
-
 from unittest.mock import MagicMock
-
 from gui.gui_controller import AnomalyDetectionGUI
 from utils.input_settings import InputSettings
 
@@ -10,6 +18,11 @@ from utils.input_settings import InputSettings
 class TestNewModelWindow(unittest.TestCase):
 
     def setUp(self):
+        """
+        Set up values before test
+        :return:
+        """
+
         self.app = AnomalyDetectionGUI()
         self.new_model_window = self.app.frames["NewModel"]
 
@@ -22,6 +35,11 @@ class TestNewModelWindow(unittest.TestCase):
         self.new_model_window.results_input.insert(0, 'path_c')
 
     def test_set_new_model_parameters(self):
+        """
+        Test valid inputs for each path
+        :return:
+        """
+
         self.app.set_features_columns_options = MagicMock(return_value={})
         self.new_model_window.set_new_model_parameters()
         self.assertEqual(self.new_model_window.training_input.get(), InputSettings.get_training_data_path())
